fd45c8706ba898ae8129727917e71d763188e6ec
erisa zaimi <ezaimi21@epoka.edu.al>
2023-12-22 17:39:03 +0100

    workflows

--- src/test/java/com/example/testingprj/ManagerTest.java
+++ src/test/java/com/example/testingprj/ManagerTest.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertFalse;
